fb10b181767486348b85a85ab6027c8ffc2c43f1
xatzos21 <xatzos21@gmail.com>
2022-08-19 10:28:03 +0200

    aj

--- aj.py
+++ aj.py
@@ -1,35 +1,28 @@
 import os
-import time
 import datetime
-import smtplib #
-from email.mime.multipart import* #
-from email.mime.text import* #
-from email.message import * #
 from colorama import *
 from art import *
 
 # initializing colorama
 init()
 
-#email function
-# def gmail_send(subject, message, from_mail, to_mail, password):  
-#     global link
-#     server = smtplib.SMTP('smtp.gmail.com', 587)
-#     server.starttls()
-#     server.login(from_mail, password)
-#     msg            = EmailMessage()
-#     message        = f'{message}'
-#     msg.set_content(message)
-#     msg['Subject'] = subject
-#     msg['From']    = from_mail
-#     msg['To']      = to_mail
-#     server.send_message(msg)
-
-new_entry = input("ENTER DIARY ENTRY HERE >>>")
-
+# time variable
 now = datetime.datetime.today().strftime("%H:%M:%S %d-%m-%Y")
 
-with open("copy.text", "a") as file:
-    file.write(now + " " + new_entry + "\n")
+print(Fore.LIGHTCYAN_EX+"")
+tprint("AWESOME JOURNAL", font="random")
+print(""+Style.RESET_ALL)
 
-    #datetime.datetime.now().isoformat(sep=" ", timespec="seconds") 
+# display current journal
+new_entry = input("ENTER DIARY ENTRY HERE >>>")
+
+here = os.getcwd().replace("\\","/")
+
+if here.upper() in new_entry.upper():
+    print("") 
+elif new_entry != "":
+    with open("journal.text", "a") as file:
+        file.write(now + " " + new_entry + "\n")
+else:
+    print("AEK")
+
